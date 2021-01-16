8395686b879fd16aac92b87580ea34e14c3b3a6f
sakura <xaatw0@gmail.com>
2021-01-17 08:07:22 +0900

    テストを作成開始

--- test/model/goto_model_test.dart
+++ test/model/goto_model_test.dart
@@ -1,3 +1,26 @@
+import 'package:flutter_app_01/model/goto_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {}
+void main() {
+  GotoModel target;
+  setUp(() {
+    target = GotoModel();
+  });
+
+  group("ベース", () {
+    test("初期値", () {
+      expect(target.getPrice(), 0);
+    });
+
+    test("価格を入れる", () {
+      expect(target.getPrice(), 0);
+      target.setPrice(100);
+      expect(target.getPrice(), 100);
+    });
+
+    test("宿泊日数", () {
+      expect(target.get);
+      target.setSay(1);
+    });
+  });
+}
