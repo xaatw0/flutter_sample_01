78e1a0c44c839b8a9ea91f80fe479a111909f5ee
sakura <xaatw0@gmail.com>
2021-01-23 10:16:15 +0900

    SelectButtonButtonを実装した

---
+++ lib/widget/atom/select_button_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SelectButtonButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text("text"),
+      onPressed: null,
+      color: Colors.lightBlueAccent,
+    );
+  }
+}
