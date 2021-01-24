46d57b07b24035a145efa44c2cf83ecf711a46d9
sakura <xaatw0@gmail.com>
2021-01-24 17:52:19 +0900

    ResultAreaの要素を並べた

--- lib/widget/organism/result_area.dart
+++ lib/widget/organism/result_area.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_01/widget/atom/result_text.dart';
 
 class ResultArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Row(
+          children: [
+            ResultText("総額:"),
+            ResultText("text"),
+          ],
+        ),
+        Row(
+          children: [
+            ResultText("割引:"),
+            ResultText("text"),
+          ],
+        ),
+        Row(
+          children: [
+            ResultText("支払い:"),
+            ResultText("text"),
+          ],
+        ),
+        Row(
+          children: [
+            ResultText("クーポン:"),
+            ResultText("text"),
+          ],
+        ),
+      ],
+    );
   }
 }
