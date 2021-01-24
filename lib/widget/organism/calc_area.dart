aa8cbdbcb7653f2914ccd31c0b75a5af01c9e956
sakura <xaatw0@gmail.com>
2021-01-24 13:51:30 +0900

    1を表示した

--- lib/widget/organism/calc_area.dart
+++ lib/widget/organism/calc_area.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_01/widget/molecule/calc_button.dart';
 import 'package:flutter_app_01/widget/molecule/function_button.dart';
 import 'package:flutter_app_01/widget/molecule/select_button.dart';
 
@@ -7,20 +9,25 @@ class CalcArea extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 3 / 4.5,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: FunctionButton(
-              Icons.clear,
-              () {},
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: FunctionButton(
+                  Icons.clear,
+                  () {},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: FunctionButton(Icons.arrow_left, () {}),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: FunctionButton(Icons.arrow_left, () {}),
-          ),
+          Expanded(child: CalButton("1", () {})),
         ],
       ),
     );
