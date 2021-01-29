ab18046074a236577bb3a297f706d39a872f4a1d
sakura <xaatw0@gmail.com>
2021-01-30 05:53:59 +0900

    CalcTemplateにOranismを配置した

--- lib/widget/template/cal_template.dart
+++ lib/widget/template/cal_template.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_01/widget/organism/calc_area.dart';
+import 'package:flutter_app_01/widget/organism/result_area.dart';
+import 'package:flutter_app_01/widget/organism/select_area.dart';
 
 class CalcTemplate extends StatelessWidget {
   @override
@@ -6,15 +9,22 @@ class CalcTemplate extends StatelessWidget {
     return Column(
       children: [
         Expanded(
-          flex: 2,
+          flex: 3,
           child: Container(
             color: Colors.white24,
+            child: Column(
+              children: [
+                SelectArea(),
+                CalcArea(),
+              ],
+            ),
           ),
         ),
         Expanded(
             flex: 1,
             child: Container(
               color: Colors.lightBlue,
+              child: ResultArea(),
             )),
       ],
     );
