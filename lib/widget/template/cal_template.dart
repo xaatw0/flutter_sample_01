9657244387e44bbc3ccb05b8efc88a475678149c
sakura <xaatw0@gmail.com>
2021-01-30 05:45:02 +0900

    CalcTemplateに枠を出す

--- lib/widget/template/cal_template.dart
+++ lib/widget/template/cal_template.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class CalcTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.white24,
+          ),
+        ),
+        Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.lightBlue,
+            )),
+      ],
+    );
   }
 }
