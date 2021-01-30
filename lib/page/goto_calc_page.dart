304a8103537190e1ff490ce9db7386d444d16887
sakura <xaatw0@gmail.com>
2021-01-31 07:11:52 +0900

    GotoCalcPageを作成し、ソースを移動した

---
+++ lib/page/goto_calc_page.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_01/model/goto_model.dart';
+import 'package:flutter_app_01/widget/template/cal_template.dart';
+import 'package:intl/intl.dart';
+
+class GotoCalcPage extends StatefulWidget {
+  GotoCalcPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _GotoCalcPageState createState() => _GotoCalcPageState();
+}
+
+class _GotoCalcPageState extends State<GotoCalcPage> {
+  GotoModel model = GotoModel();
+  int _price = 0;
+  int _person = 1;
+  int _saty = 0;
+  String text = "";
+
+  int _minus = 0;
+  int _coupon = 0;
+  int _pay = 0;
+
+  final formatter = NumberFormat("#,###");
+
+  @override
+  Widget build(BuildContext context) {
+    List<DropdownMenuItem> stayList = List.generate(
+      7,
+      (index) => DropdownMenuItem(
+        child: Text("${index + 1}泊${index + 2}日"),
+        value: index + 1,
+      ),
+    );
+
+    List<DropdownMenuItem> peopleList = List.generate(
+      10,
+      (index) => DropdownMenuItem(
+        child: Text("${index + 1}人"),
+        value: index + 1,
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: CalcTemplate(),
+      /*
+      Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Text(
+                      "宿泊日数",
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: DropdownButton(
+                    value: _saty,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("日帰り"),
+                        value: 0,
+                      ),
+                      ...stayList,
+                      DropdownMenuItem(
+                        child: Text("8泊以上"),
+                        value: 10,
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        _saty = value;
+                        model.setStay(value);
+                        _calc();
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Text(
+                      "宿泊人数",
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: DropdownButton(
+                    value: _person,
+                    items: peopleList,
+                    onChanged: (value) {
+                      setState(() {
+                        _person = value;
+                        model.setPerson(value);
+                        _calc();
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            NumericKeyboard(
+              onKeyboardTap: _onKeyboardTap,
+              textColor: Colors.red,
+              rightButtonFn: () {
+                setState(() {
+                  text = text.substring(0, text.length - 1);
+                  model.setPrice(int.parse(text));
+                });
+              },
+              rightIcon: Icon(
+                Icons.backspace,
+                color: Colors.red,
+              ),
+              leftButtonFn: () {
+                print('left button clicked');
+              },
+              leftIcon: Icon(
+                Icons.check,
+                color: Colors.red,
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Text(
+                      "料金",
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Text("${formatter.format(_price)}"),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Text(
+                      "割引",
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Text("${formatter.format(_minus)}"),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Text(
+                      "支払額",
+                      textAlign: TextAlign.right,
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    "${formatter.format(_pay)}",
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: Text(
+                      "クーポン",
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Text("${formatter.format(_coupon)}"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),*/
+    );
+  }
+
+  _onKeyboardTap(String value) {
+    setState(() {
+      text = text + value;
+      _calc();
+    });
+  }
+
+  _calc() {
+    _price = int.parse(text);
+    model.setPrice(_price);
+
+    _minus = model.getMinus();
+    _coupon = model.getCoupone();
+    _pay = model.getPay();
+  }
+}
