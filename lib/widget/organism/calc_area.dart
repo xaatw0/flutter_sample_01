8424cb4a337d68e500942acc33b0f706674a78df
sakura <xaatw0@gmail.com>
2021-01-31 08:31:38 +0900

    riverpodが動いた

--- lib/widget/organism/calc_area.dart
+++ lib/widget/organism/calc_area.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_01/model/calc_model.dart';
 import 'package:flutter_app_01/widget/molecule/calc_button.dart';
 import 'package:flutter_app_01/widget/molecule/calc_flat_button.dart';
 import 'package:flutter_app_01/widget/molecule/function_button.dart';
 import 'package:flutter_app_01/widget/molecule/select_button.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:state_notifier/state_notifier.dart';
 
-class CalcArea extends StatelessWidget {
+import 'function_area.dart';
+
+class Price extends StateNotifier<int> {
+  Price() : super(0);
+  void setPrice(int price) => state = price;
+}
+
+final priceProvider = StateNotifierProvider((_) => Price());
+
+class CalcArea extends HookWidget {
+  CalcModel model = CalcModel();
+
   @override
   Widget build(BuildContext context) {
+    final state = useProvider(priceProvider.state);
+    final price = useProvider(priceProvider);
+
     return AspectRatio(
       aspectRatio: 3 / 4.5,
       child: Column(
         children: [
           Expanded(flex: 1, child: FunctionArea()),
-          for (int i = 0; i < 3; i++) Expanded(flex: 2, child: CalAreaRow(i)),
+          for (int i = 0; i < 3; i++)
+            Expanded(flex: 2, child: CalAreaRow(model, i)),
           Expanded(
             flex: 2,
             child: Row(
@@ -22,7 +41,10 @@ class CalcArea extends StatelessWidget {
                     flex: 2,
                     child: Padding(
                         padding: EdgeInsets.all(8),
-                        child: CalcFlatButton("0", () {}))),
+                        child: CalcFlatButton(state.toString(), () {
+                          model.push(CalcKey.KEY_1);
+                          price.setPrice(model.value);
+                        }))),
                 Expanded(
                     flex: 1,
                     child: Padding(
@@ -38,42 +60,19 @@ class CalcArea extends StatelessWidget {
   }
 }
 
-class FunctionArea extends StatelessWidget {
-  const FunctionArea({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 8.0),
-          child: FunctionButton(
-            Icons.clear,
-            () {},
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: FunctionButton(Icons.arrow_left, () {}),
-        ),
-      ],
-    );
-  }
-}
-
-class CalAreaRow extends StatelessWidget {
+class CalAreaRow extends HookWidget {
   final int index;
+  final CalcModel model;
 
-  const CalAreaRow(
+  CalAreaRow(
+    this.model,
     this.index, {
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final price = useProvider(priceProvider);
     return Row(
       children: [
         for (int i = 0; i < 3; i++)
@@ -82,7 +81,11 @@ class CalAreaRow extends StatelessWidget {
               padding: EdgeInsets.all(8),
               child: CalcButton(
                 (i + 1 + 3 * (2 - index)).toString(),
-                () {},
+                () {
+                  int value = i + 1 + 3 * (2 - index);
+                  model.push(CalcKey.values[value]);
+                  price.setPrice(model.value);
+                },
               ),
             ),
           ),
--- lib/widget/organism/result_area.dart
+++ lib/widget/organism/result_area.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_01/widget/atom/result_accent_text.dart';
 import 'package:flutter_app_01/widget/atom/result_text.dart';
 import 'package:flutter_app_01/widget/molecule/result_area_row.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ResultArea extends StatelessWidget {
+import 'calc_area.dart';
+
+class ResultArea extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final state = useProvider(priceProvider.state);
     return Column(
       children: [
         ResultAreaRow(
           ResultText("総額:"),
-          ResultText("123,456"),
+          ResultText(state.toString()),
         ),
         ResultAreaRow(
           ResultText("割引:"),
