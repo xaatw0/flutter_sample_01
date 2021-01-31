51c85d610c196a19b7286410b4491dbf2350052e
sakura <xaatw0@gmail.com>
2021-01-31 09:23:54 +0900

    riverpodの値で画面に表示できた

--- lib/widget/organism/calc_area.dart
+++ lib/widget/organism/calc_area.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_01/model/calc_model.dart';
+import 'package:flutter_app_01/notifier/price_notifier.dart';
 import 'package:flutter_app_01/widget/molecule/calc_button.dart';
 import 'package:flutter_app_01/widget/molecule/calc_flat_button.dart';
 import 'package:flutter_app_01/widget/molecule/function_button.dart';
 import 'package:flutter_app_01/widget/molecule/select_button.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
 
 import 'function_area.dart';
+
+final priceProvider = StateNotifierProvider((_) => PriceNotifer());
 
 class CalcArea extends HookWidget {
   CalcModel model = CalcModel();
 
   @override
   Widget build(BuildContext context) {
+    final state = useProvider(priceProvider.state);
+
     return AspectRatio(
       aspectRatio: 3 / 4.5,
       child: Column(
@@ -29,7 +35,7 @@ class CalcArea extends HookWidget {
                     flex: 2,
                     child: Padding(
                         padding: EdgeInsets.all(8),
-                        child: CalcFlatButton("0", () {}))),
+                        child: CalcFlatButton(state.toString(), () {}))),
                 Expanded(
                     flex: 1,
                     child: Padding(
@@ -57,6 +63,8 @@ class CalAreaRow extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final price = useProvider(priceProvider);
+
     return Row(
       children: [
         for (int i = 0; i < 3; i++)
@@ -68,6 +76,7 @@ class CalAreaRow extends HookWidget {
                 () {
                   int value = i + 1 + 3 * (2 - index);
                   model.push(CalcKey.values[value]);
+                  price.setPrice(model.value);
                 },
               ),
             ),
