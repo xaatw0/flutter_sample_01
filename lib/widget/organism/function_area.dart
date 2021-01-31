aed48865b43d35115f5f2da29a2a56e563b9ef03
sakura <xaatw0@gmail.com>
2021-01-31 13:45:03 +0900

    削除ボタン、クリアボタンも有効になるようにした

--- lib/widget/organism/function_area.dart
+++ lib/widget/organism/function_area.dart
@@ -3,14 +3,18 @@ import 'package:flutter_app_01/model/calc_model.dart';
 import 'package:flutter_app_01/widget/molecule/function_button.dart';
 import 'package:flutter_app_01/widget/molecule/select_button.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/all.dart';
+import 'package:flutter_app_01/notifier/price_notifier.dart';
 
 class FunctionArea extends HookWidget {
   final CalcModel model;
-
   FunctionArea(this.model);
 
   @override
   Widget build(BuildContext context) {
+    final price = useProvider(priceProvider);
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -18,12 +22,18 @@ class FunctionArea extends HookWidget {
           padding: const EdgeInsets.only(right: 8.0),
           child: FunctionButton(
             Icons.clear,
-            () {},
+            () {
+              model.push(CalcKey.KEY_CLEAR);
+              price.setPrice(model.value);
+            },
           ),
         ),
         Padding(
           padding: const EdgeInsets.only(left: 8.0),
-          child: FunctionButton(Icons.arrow_left, () {}),
+          child: FunctionButton(Icons.arrow_left, () {
+            model.push(CalcKey.KEY_DEL);
+            price.setPrice(model.value);
+          }),
         ),
       ],
     );
