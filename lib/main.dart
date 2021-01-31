f468e03081b658b2365d0bb220ee8267ba53fa29
sakura <xaatw0@gmail.com>
2021-01-31 09:01:40 +0900

    ProviderScopeを採用した

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:flutter_app_01/widget/organism/function_area.dart';
 import 'package:flutter_app_01/widget/organism/result_area.dart';
 import 'package:flutter_app_01/widget/organism/select_area.dart';
 import 'package:flutter_app_01/widget/template/cal_template.dart';
-import 'package:flutter_riverpod/all.dart';
+import 'package:hooks_riverpod/all.dart';
 import 'package:numeric_keyboard/numeric_keyboard.dart';
 import 'package:intl/intl.dart';
 
