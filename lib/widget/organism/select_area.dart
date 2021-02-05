4b41a7dfb9c303c263085b53bb0318149d14b203
sakura <xaatw0@gmail.com>
2021-02-06 08:23:36 +0900

    resultProviderを追加

--- lib/widget/organism/select_area.dart
+++ lib/widget/organism/select_area.dart
@@ -2,6 +2,7 @@ import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_01/model/goto_model.dart';
 import 'package:flutter_app_01/notifier/int_value_notifier.dart';
+import 'package:flutter_app_01/notifier/result_notifier.dart';
 import 'package:flutter_app_01/widget/molecule/select_button.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/all.dart';
@@ -23,6 +24,7 @@ class SelectArea extends HookWidget {
     final peopleState = useProvider(peopleProvider.state);
     final stay = useProvider(stayProvider);
     final stayState = useProvider(stayProvider.state);
+    final result = useProvider(resultProvider);
 
     return Row(
       children: [
@@ -34,13 +36,13 @@ class SelectArea extends HookWidget {
                 Icons.face,
                 lstPeople[peopleState],
                 () async {
-                  int result = await showConfirmationDialog<int>(
+                  int index = await showConfirmationDialog<int>(
                     context: context,
                     title: '人数',
                     message: '宿泊する人数を選んでください',
                     actions: createDialogSelect(lstPeople),
                   );
-                  people.setValue(result);
+                  people.setValue(index);
                 },
               ),
             )),
