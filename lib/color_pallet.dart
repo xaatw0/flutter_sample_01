d24ffe6f4c4aec2ad25595efce5ca9d60b5a1531
sakura <xaatw0@gmail.com>
2021-02-06 11:41:07 +0900

    色のベースクラスを追加した

---
+++ lib/color_pallet.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColorPallet {
+  final Color base;
+  final Color accent;
+  final Color sub;
+  final Color background;
+
+  ColorPallet(this.base, this.accent, this.sub, this.background);
+}
