7d9efbb89d4bd3c28fb1e06094ff21e5a07e3e67
sakura <xaatw0@gmail.com>
2021-02-14 06:36:58 +0900

    テーマ切り替え用のボタンの仮実装

--- lib/widget/organism/ThemeButton.dart
+++ lib/widget/organism/ThemeButton.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_app_01/widget/atom/theme_button_button.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter/material.dart';
 
 class ThemeButton extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return ThemeButtonButton(
+      Icon(Icons.wb_sunny),
+      () {},
+    );
   }
 }
