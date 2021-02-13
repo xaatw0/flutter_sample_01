2306b73d6354c80fe65f3240701be112141d4c6f
sakura <xaatw0@gmail.com>
2021-02-14 05:54:56 +0900

    ダークテーマ用の色を追加

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,10 @@
 // ベースの色の定義
-const kColorBase = "#9cc9e5";
-const kColorAccent = "#cae5d3";
-const kColorSub = "#dfe2ea";
-const kColorBackground = kColorSub;
+const kColorBase = "#F7905B";
+const kColorAccent = "#EBE7E6";
+const kColorSub = "#F5F5F5";
+const kColorBackground = "#F5F5F5";
+
+const kColorDarkBase = "#A6A6A6";
+const kColorDarkAccent = "#FF9402";
+const kColorDarkSub = "#333333";
+const kColorDarkBackground = "#333333";
