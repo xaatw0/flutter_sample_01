0662f312e5404d37a07c4aef611dde360dfefc24
sakura <xaatw0@gmail.com>
2021-02-21 06:48:57 +0900

    setupallを実装した

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -4,5 +4,18 @@ import 'package:flutter_driver/flutter_driver.dart';
 import "package:test/test.dart";
 
 void main() {
-  group("1を押す", () {});
+  group("1を押す", () {
+    final key1Finder = find.byValueKey("1");
+    final priceValueFinder = find.byValueKey("price_value");
+
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+      final health = await driver.checkHealth();
+      if (health.status == HealthStatus.bad) {
+        fail("Flutter driver が起動せず");
+      }
+    });
+  });
 }
