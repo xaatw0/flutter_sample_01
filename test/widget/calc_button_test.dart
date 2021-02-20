61964e5dcee362421de7b9ebb0917c12d04e0db0
sakura <xaatw0@gmail.com>
2021-02-20 10:59:18 +0900

    テストを追加した

--- test/widget/calc_button_test.dart
+++ test/widget/calc_button_test.dart
@@ -3,12 +3,48 @@ import 'package:flutter_app_01/color_pallet.dart';
 import 'package:flutter_app_01/widget/molecule/calc_button.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void min() {
+void main() {
   testWidgets("CalcButtonのテスト1", (WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(
         home: CalcButton("1", null, lightThema),
       ),
     );
+
+    await tester.pump();
+    expect(find.text("1"), findsOneWidget);
+    expect(find.text("2"), findsNothing);
+  });
+  testWidgets("CalcButtonのテスト2", (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: CalcButton("2", null, lightThema),
+      ),
+    );
+
+    await tester.pump();
+    expect(find.text("2"), findsOneWidget);
+    expect(find.text("1"), findsNothing);
+  });
+
+  testWidgets("CalcButtonのonTap", (WidgetTester tester) async {
+    int number = 0;
+    var onTap = () {
+      number++;
+    };
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: CalcButton("1", onTap, lightThema),
+      ),
+    );
+
+    await tester.pump();
+
+    expect(number, 0);
+    await tester.tap((find.text("1")));
+    expect(number, 1);
+    await tester.tap((find.text("1")));
+    expect(number, 2);
   });
 }
